86af5d6dee6b40091b2fac4421180717ee6ffd30
helt-vilt <andre.lofaldli@gmail.com>
2014-02-24 12:26:13 +0100

    Update main.go

--- prosjekt/src/main.go
+++ prosjekt/src/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-
-
+."ControlModule"
+."DriverModule"
+."IOModule"
+."NetworkModule"
 )
 
 func main() {
-
-
+go InitNetworkModule(LocalIP, port, netIn, netOut)
+// NEI!!!
 
 }
