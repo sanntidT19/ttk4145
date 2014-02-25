69dec571103edce171ef6aee575c761450714d43
sjokolAda <leif_halvor@hotmail.com>
2014-02-25 15:56:06 +0100

    yolo

--- ov5/src/driver/elev.go
+++ ov5/src/driver/elev.go
@@ -1,23 +1,31 @@
 package driver
 
-const N_FLOORS := 4
+
+import (
+	"Matrix"
+	"math"
+	"C"
+)
+
+const N_FLOORS = 4
 
 type elev_button_type_t int
 const (
 	BUTTON_CALL_UP elev_button_type_t = iota
-	BUTTON_CALL_DOWN elev_button_type_t
-	BUTTON_COMMAND elev_button_type_t
+	BUTTON_CALL_DOWN elev_button_type_t = iota
+	BUTTON_COMMAND elev_button_type_t = iota
 )
-
+lamp_channel_matrix := NewMatrix(4,3)
+button_channel_matrix := NewMatrix(4,3)
 //TODO: lampematrise
 
 func elev_init() int {
-	if !io_init() {
+	if io_init() == 0{
 		//feil
 		return 0
 	}
 	
-	for ( i := 0; i < N_FLOORS; i++){
+	for i := 0; i < N_FLOORS; i++{
 		if ( i != 0){
 			elev_set_button_lamp(BUTTON_CALL_DOWN, i, 0)
 		}
@@ -35,7 +43,7 @@ func elev_init() int {
 	return 1
 }
 
-func elev_set_Speed(int speed, int lastSpeed) int { //returns last speed
+func elev_set_speed(speed int, last_speed int) int { //returns last speed
 	if speed > 0 {
 		io_clear_bit(MOTORDIR)
 	} else if (speed < 0){
@@ -46,19 +54,19 @@ func elev_set_Speed(int speed, int lastSpeed) int { //returns last speed
 		io_set_bit(MOTORDIR)
 	}
 	
-	io_write_analog(MOTOR, 2048 + 4*abs(speed))
+	io_write_analog(MOTOR, 2048 + 4*int(math.Abs(float64(speed))))
 	
 	return speed
 }
   
 func elev_get_floor_sensor_signal() int {
-	if (io_read_bit(SENSOR1)){
+	if (io_read_bit(SENSOR1) == 1){
 		return 0
-	} else if (io_read_bit(SENSOR2)) {
+	} else if (io_read_bit(SENSOR2) == 1) {
 		return 1
-	} else if (io_read_bit(SENSOR3)) {
+	} else if (io_read_bit(SENSOR3) == 1) {
 		return 2
-	} else if (io_read_bit(SENSOR4)) {
+	} else if (io_read_bit(SENSOR4) == 1) {
 		return 3
 	} else {
 		return -1
@@ -71,8 +79,8 @@ func elev_get_button_signal(button elev_button_type_t,floor int) {
 	assert(!(button == BUTTON_CALL_UP && floor == N_FLOORS-1))
 	assert(!(button == BUTTON_CALL_DOWN && floor == 0))
 	assert( button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND)
-	
-	if io_read_bit(button_channel_matrix[floor][button]) == 1 {
+
+	if io_read_bit( int(C.button_channel_matrix[ C.int(floor) ][ C.int(button) ]) )  == 1 {
 		return 1
 	} else {
 		return 0
@@ -110,6 +118,7 @@ func elev_set_button_lamp(button elev_button_type_t, floor int, value int) {
 	assert( button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND)
 	
 	
+	
 }
 func elev_set_stop_lamp(value int) {
 	if value != 0 {
@@ -132,3 +141,59 @@ func assert(t bool) {
 		log.Panic("assertion failed!")
 	}
 }
+
+func initMatrices() {
+	lamp_channel_matrix.Set(0,0,LIGHT_UP1)
+	lamp_channel_matrix.Set(0,1,LIGHT_DOWN1)
+	lamp_channel_matrix.Set(0,1,LIGHT_COMMAND1)
+	
+	lamp_channel_matrix.Set(1,0,LIGHT_UP2)
+	lamp_channel_matrix.Set(1,1,LIGHT_DOWN2)
+	lamp_channel_matrix.Set(1,2, LIGHT_COMMAND2)
+	
+	lamp_channel_matrix.Set(2,0,LIGHT_UP3)
+	lamp_channel_matrix.Set(2,1,LIGHT_DOWN3)
+	lamp_channel_matrix.Set(2,2, LIGHT_COMMAND3)
+	
+	lamp_channel_matrix.Set(3,0,LIGHT_UP4)
+	lamp_channel_matrix.Set(3,1,LIGHT_DOWN4)
+	lamp_channel_matrix.Set(3,2,LIGHT_COMMAND4)
+	
+	
+	button_channel_matrix.Set(0,0,FLOOR_UP1)
+	button_channel_matrix.Set(0,1,FLOOR_DOWN1)
+	button_channel_matrix.Set(0,1,FLOOR_COMMAND1)
+	
+	button_channel_matrix.Set(1,0,FLOOR_UP2)
+	button_channel_matrix.Set(1,1,FLOOR_DOWN2)
+	button_channel_matrix.Set(1,2, FLOOR_COMMAND2)
+	
+	button_channel_matrix.Set(2,0,FLOOR_UP3)
+	button_channel_matrix.Set(2,1,FLOOR_DOWN3)
+	button_channel_matrix.Set(2,2, FLOOR_COMMAND3)
+	
+	button_channel_matrix.Set(3,0,FLOOR_UP4)
+	button_channel_matrix.Set(3,1,FLOOR_DOWN4)
+	button_channel_matrix.Set(3,2,FLOOR_COMMAND4)
+}
+
+
+
+/*
+static const int lamp_channel_matrix[N_FLOORS][N_BUTTONS] = {
+    {LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
+    {LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
+    {LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
+    {LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
+};
+  
+  
+
+static const int button_channel_matrix[N_FLOORS][N_BUTTONS] = {
+    {FLOOR_UP1, FLOOR_DOWN1, FLOOR_COMMAND1},
+    {FLOOR_UP2, FLOOR_DOWN2, FLOOR_COMMAND2},
+    {FLOOR_UP3, FLOOR_DOWN3, FLOOR_COMMAND3},
+    {FLOOR_UP4, FLOOR_DOWN4, FLOOR_COMMAND4},
+};
+
+*/
--- ov5/src/driver/io.go
+++ ov5/src/driver/io.go
@@ -1,9 +1,9 @@
 package driver
 
 /*
-#cgo LDFLAGS: -lcomedi -lm
+#cgo LDFLAGS: -lcomedi -lm -std=c99
 #include "io.h"
-typedef int h
+
 */
 import "C"
 
@@ -11,22 +11,22 @@ func io_init() int {
   return int(C.io_init());
 }
 
-func io_set_bit(int channel){
-  C.io_set_bit(C.h(channel))
+func io_set_bit(channel int){
+  C.io_set_bit(C.int(channel))
 }
 
-func io_clear_bit(int channel){
-  C.io_clear_bit(C.h(int channel))
+func io_clear_bit(channel int){
+  C.io_clear_bit(C.int(channel))
 }
 
-func io_write_analog(int channel, int value) {
+func io_write_analog(channel int, value int) {
   C.io_write_analog(C.int(channel), C.int(value))
 }
 
-func io_read_bit(int channel) {
-  C.io_read_bit(C.h(channel))
+func io_read_bit(channel int) int {
+  return int(C.io_read_bit(C.int(channel)))
 }
 
-func io_read_analog(int channel) {
-  C.io_read_analog(C.int(channel))
+func io_read_analog(channel int) int {
+  return int(C.io_read_analog(C.int(channel)))
 }
