2d33b8d3c7a2e54d34c8ebcf62edf8760487ef57
helt-vilt <andre.lofaldli@gmail.com>
2014-03-05 17:42:10 +0100

    Update and rename io.go to IO.go

---
+++ prosjekt/src/DriverModule/IO.go
@@ -0,0 +1,33 @@
+package DriverModule
+
+
+/*
+#cgo LDFLAGS: -lcomedi -lm -std=c99
+#include "io.h"
+
+*/
+import "C"
+
+func IOInit() int {
+	return int(C.io_init());
+}
+
+func IOSetBit(channel int){
+	C.io_set_bit(C.int(channel))
+}
+
+func IOClearBit(channel int){
+	C.io_clear_bit(C.int(channel))
+}
+
+func IOWriteAnalog(channel int, value int) {
+	C.io_write_analog(C.int(channel), C.int(value))
+}
+
+func IOReadBit(channel int) int {
+	return int(C.io_read_bit(C.int(channel)))
+}
+
+func IOReadAnalog(channel int) int {
+	return int(C.io_read_analog(C.int(channel)))
+}
--- prosjekt/src/DriverModule/io.go
+++
@@ -1,3 +0,0 @@
-package DriverModule
-
-import "C"
